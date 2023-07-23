7dc633c30d7ce54d15961c14a3891fbfb1a4719a
shreeraj007 <shreerajmane007@gmail.com>
2023-07-23 14:07:00 +0530

    updated hero-section

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -35,9 +35,9 @@ const HeroBanner = () => {
                         src="/slide-1.png"
                         className="aspect-[16/10] md:aspect-auto object-cover"
                     />
-                    <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
+                    {/* <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
                         View More
-                    </div>
+                    </div> */}
                 </div>
 
                 <div>
